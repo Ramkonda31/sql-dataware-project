2dafe121d10a3c2665e41feeed8dc0aacc769e95
Ramkonda31 <91202956+Ramkonda31@users.noreply.github.com>
2025-03-04 20:08:15 -0500

    Create init.database.sql

---
+++ scripts/init.database.sql
@@ -0,0 +1,16 @@
+-- created database named DataWarehouse
+
+CREATE DATABASE DataWarehouse;
+
+
+USE DataWarehouse;
+
+-- created schemas named bronze, silver, gold
+CREATE SCHEMA bronze;
+
+
+CREATE SCHEMA silver;
+
+
+
+CREATE SCHEMA gold;
